9d2d53dc1d1898317a4af5cfd94e29bceb2ab23e
Eddy Martin <eddypmartin@gmail.com>
2022-11-23 13:34:34 -0500

    c16 adapter front-page.ph pour l'affichage des article de catégorie accueil

--- front-page.php
+++ front-page.php
@@ -29,18 +29,28 @@ get_header();
 	<?php
 		if ( have_posts() ) :
 			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post(); ?>
+			while ( have_posts() ) : the_post(); ?>
 				<article class="grille__article">
-			<h6><?= get_the_title(); ?></h6>
-			------------------
+			<?php
+			 $titre = get_the_title();
+			 $code_cours = substr($titre,0,7);
+			 $heure_cours = substr($titre,strrpos($titre,'('));
+			 $titre = substr($titre,8);
+			 $longueur = strlen($titre);
+
+			 //$titre = substr($titre, strrpos($titre,'(') - strlen($titre));
+			 $titre = substr($titre, 0, strrpos($titre,'(') -  $longueur );
+			 ?>
+		
 			<?php // echo (is_category('galerie')) ?>
 
 			<?php 
 			$le_permalien = "<a href='" . get_the_permalink() . "'>Suite</a>";
 			?>
-			
-			<p><?= wp_trim_words(get_the_excerpt(), 20,$le_permalien) ; ?></p>
+			<h6><?=  $titre;  ?></h6>
+			<p><?= wp_trim_words(get_the_excerpt(), 10,$le_permalien) ; ?></p>
+			<p>Type de cours:<?php the_field("type_de_cours") ?></p>
+			<p>TIM - Collège de Maisonneuve</p>
 			
 
 			</article>
